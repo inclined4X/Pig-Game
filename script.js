5377a20b5a7b983eb0b43b4d9c082ad44786c6c7
inclined4X <jezemiahsam48@gmail.com>
2024-01-31 11:12:02 +0000

    updated the selected elements by adding the buttons for new game, roll and hold

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEL = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //setting the scores to zero and starting conditions
 score0El.textContent = 0;
