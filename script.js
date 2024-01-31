730d673eb33ea78ca9990895eb90ed883332df96
inclined4X <jezemiahsam48@gmail.com>
2024-01-31 11:26:54 +0000

    added a random dice roll and linked it with the image that displayes when the dice button is clicked on

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden');
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1.Generate a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEL.classList.remove('hidden');
+  diceEL.src = `dice-${dice}.png`;
+
+  // 3. check for rolled 1: if true, switch to next player
+});
