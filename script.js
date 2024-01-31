f9d272858811b83452d4f404c12a797a1393cdfc
inclined4X <jezemiahsam48@gmail.com>
2024-01-31 11:42:58 +0000

    added the player 1 and player 2 element variables and the updated the score of player to current score which is the current score + dice

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 //Getting the ids and selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEL = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -12,6 +14,9 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEL.classList.add('hidden');
+
+// storing the current score outside the btn event to prevent resetting the current score everytime the btn roll is clicked on
+let currentScore = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -24,4 +29,11 @@ btnRoll.addEventListener('click', function () {
   diceEL.src = `dice-${dice}.png`;
 
   // 3. check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // add dice to the current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // Switch to next player
+  }
 });
