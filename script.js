462e4cbb3f95e514f06df87a39bb330a373196be
inclined4X <jezemiahsam48@gmail.com>
2024-02-01 00:42:22 +0000

    did the playing if statement for the btnhold

--- script.js
+++ script.js
@@ -67,26 +67,26 @@ btnRoll.addEventListener('click', function () {
 });
 
 btnHold.addEventListener('click', function () {
-  // 1. Add current score to active player's score
-  scores[activePlayer] += currentScore;
-  // scores[1] = scores[1] + currentScore
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  // 2. check if player's score is >=100
-  if (scores[activePlayer] >= 20) {
-    // finish game
-    playing = false;
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
-  } else {
-    // switch to the next player when the game finishes
-    switchPlayer();
+    // 2. check if player's score is >=100
+    if (scores[activePlayer] >= 20) {
+      // finish game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch to the next player when the game finishes
+      switchPlayer();
+    }
   }
-
-  //switch to the next player
 });
